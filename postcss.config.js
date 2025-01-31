5f2c54e03e6957026b416a2a33560c110927225e
wtf403 <wtf403@yandex.ru>
2025-01-31 08:49:12 +0300

    fix gh-pages

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: [
+    require("postcss-preset-env")({
+      stage: 1,
+      features: {
+        "nesting-rules": true,
+      },
+    }),
+    require("autoprefixer"),
+  ],
+};
